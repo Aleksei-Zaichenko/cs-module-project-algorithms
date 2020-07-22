220c4e09e8eae2bbf3f2954b9bb4691efb45c366
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-07-22 13:09:20 -0400

    Adds solution for single_number.py

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,26 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+
+
 def single_number(arr):
     # Your code here
+    numberOfAppearances = {}
+    singleNumber = 0
 
-    pass
+    for item in range (len(arr)):
+        number = arr[item]
+        if number in numberOfAppearances:
+            numberOfAppearances[number] = 2
+        else:
+            numberOfAppearances[number] = 1
+
+
+    for x in numberOfAppearances:
+        if numberOfAppearances[x] == 1:
+            singleNumber = x
+            
+    return singleNumber
 
 
 if __name__ == '__main__':
