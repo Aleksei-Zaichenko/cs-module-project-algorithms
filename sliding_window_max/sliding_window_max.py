de35f5cebfbf804d56ff3cc1d1c8f4f926e82f7a
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-07-22 20:20:38 -0400

    Adds sliding_window_nax function definition in sliding_window_max.py

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,15 +2,35 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
-def sliding_window_max(nums, k):
-    # Your code here
 
-    pass
+
+def sliding_window_max(nums, k):
+    result = []
+    tempList = []
+
+    for i in range( len(nums)- k + 1 ):
+        max = nums[i]
+        
+        # tempList will containt k number of elements
+        # for example if k = 3, templist will containt 3 elements
+        tempList.extend(nums[i:k+i])
+
+        # then we choose the largest element from tempList
+        for number in tempList:
+            if number > max:
+                max = number
+
+        # add max value to result
+        result.append(max)
+        # clear temp list that containt k number of elements
+        tempList = []
+    return result
 
 
 if __name__ == '__main__':
-    # Use the main function here to test out your implementation 
+    # Use the main function here to test out your implementation
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
 
-    print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
+    print(
+        f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
